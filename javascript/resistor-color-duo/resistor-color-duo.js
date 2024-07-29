5787f2931bd78c50e3c57ea25b51bf2077292827
Kye Davey <kye.j.davey@gmail.com>
2024-07-29 12:51:53 +1000

    add new exercise

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (resistorColors) => {
+  return Number(
+    "" + COLORS.indexOf(resistorColors[0]) + COLORS.indexOf(resistorColors[1])
+  );
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
